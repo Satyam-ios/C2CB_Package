31e5a9ff5af098420a7b3fdb9fe0b9ad1fb45e52
Satyam Kumar <satyam.kumar@vgroup.net>
2025-04-10 19:56:39 +0530

    update

--- Package.swift
+++ Package.swift
@@ -5,16 +5,30 @@ import PackageDescription
 
 let package = Package(
     name: "C2CB_iOS_Library",
+    platforms: [
+            .iOS(.v13)
+        ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "C2CB_iOS_Library",
             targets: ["C2CB_iOS_Library"]),
     ],
+    dependencies: [
+            .package(url: "https://github.com/twilio/twilio-voice-ios", from: "6.13.0")
+        ],
     targets: [
-           .binaryTarget(
-               name: "C2CB_iOS_Library",
-               path: "C2CB_iOS_Library.xcframework"
-           )
+        .target(
+                name: "C2CB_iOS_Library",
+                dependencies: [
+                    "twilio-voice-ios",
+                    "C2CB_iOS_Library"
+                ],
+                path: "Sources",
+               ),
+        .binaryTarget(
+            name: "C2CB_iOS_Library",
+            path: "./C2CB_iOS_Library.xcframework"
+        ),
        ]
 )
