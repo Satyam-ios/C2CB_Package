827f1040edef7049c1b91f21d3fac46847f262e8
Satyam Kumar <satyam.kumar@vgroup.net>
2025-04-11 11:31:32 +0530

    update package

--- Package.swift
+++ Package.swift
@@ -6,29 +6,30 @@ import PackageDescription
 let package = Package(
     name: "C2CB_iOS_Library",
     platforms: [
-            .iOS(.v13)
-        ],
+        .iOS(.v13)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "C2CB_iOS_Library",
-            targets: ["C2CB_iOS_Library_Package"]),
+            targets: ["C2CB_iOS_LibraryWrapper"]
+        ),
     ],
     dependencies: [
-            .package(url: "https://github.com/twilio/twilio-voice-ios", from: "6.13.0")
-        ],
+        .package(url: "https://github.com/twilio/twilio-voice-ios", from: "6.13.0")
+    ],
     targets: [
+        // Wrapper target to expose both TwilioVoice and your xcframework
         .target(
-                name: "C2CB_iOS_Library_Package",
-                dependencies: [
-                    "twilio-voice-ios",
-                    "C2CB_iOS_Library"
-                ],
-                path: "Sources",
-               ),
+            name: "C2CB_iOS_LibraryWrapper",
+            dependencies: [
+                "twilio-voice-ios",
+                "C2CB_iOS_Library"
+            ]
+        ),
+        // Your binary framework
         .binaryTarget(
             name: "C2CB_iOS_Library",
             path: "./C2CB_iOS_Library.xcframework"
         ),
-       ]
+    ]
 )
---
+++ Sources/C2CB_iOS_LibraryWrapper/EmptyFile.swift
@@ -0,0 +1,10 @@
+//
+//  EmptyFile.swift
+//  C2CB_Package
+//
+//  Created by V Group on 11/04/25.
+//
+
+
+
+
